3b1e0e731cf1275a4355b16c9d2239610cba7913
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2025-04-17 11:38:29 +0200

    refactor code: create stats.py file and move count_nums function
    import the count_ num function in the main.py file

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book_path = "books/frankenstein.txt"
