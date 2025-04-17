cdc18d555e677a621144a1c52765bf005326487b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2025-04-17 12:56:39 +0200

    create a char_count function
    - that counts number of characters
    - import count_chars function in main.py
    -

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import count_words
-
+from stats import count_characters
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
@@ -10,6 +10,9 @@ def main():
     text = get_book_text(book_path)
     num_of_words = count_words(text)
     print(f"{num_of_words} words found in the document")
+
+    char_counts = count_characters(text)
+    print(char_counts)
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    text = text.lower()
+    chars = {}
+    for char in text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+
+    
