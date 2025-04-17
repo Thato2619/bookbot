8e7b0204a77c4bf78b58d5ab7077f6898ea9524b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2025-04-17 11:23:07 +0200

    create a two functions
    - one function for file path
    - one function that utilizes the logic of the first one

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    content = get_book_text(book_path)
+    print(content)
+
+
+main()
